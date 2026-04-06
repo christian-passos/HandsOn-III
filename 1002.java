fa44c971ff7101630926a2a39fd21673088709f6
Christian Passos <christiangfotos@gmail.com>
2026-04-06 20:38:30 -0300

    1002.java

---
+++ 1002.java
@@ -0,0 +1,17 @@
+import java.io.IOException;
+import java.util.Scanner;
+
+public class 1002 {
+ 
+    public static void main(String[] args) throws IOException {
+    
+    Scanner sc = new Scanner(System.in);
+    double n= 3.14159, raio, area;
+    
+    raio = sc.nextDouble();
+    area = n * (raio * raio);
+    
+    System.out.printf("A=%.4f\n",area);
+    sc.close();
+    }
+}
